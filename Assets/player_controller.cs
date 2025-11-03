f0c7381a9faeb91c5ce59c6f0326d8733045776b
kyleknopp-creator <kyle.knopp@discoverystudent.org>
2025-11-03 08:52:44 -0500

    11/03

---
+++ Assets/player_controller.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Untyengine.InputSystem;
+public class player_controller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
